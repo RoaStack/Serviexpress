7ad6e5812029ae5ad560847389874095a3b23455
RoaStack <diegoroa177@gmail.com>
2025-10-30 02:37:46 -0300

    agregando modelos a admin

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import OrdenPedido, DetalleOrden
 
-# Register your models here.
+class DetalleOrdenInline(admin.TabularInline):
+    model = DetalleOrden
+    extra = 1
+
+@admin.register(OrdenPedido)
+class OrdenPedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'proveedor', 'mecanico', 'fecha', 'monto_total')
+    inlines = [DetalleOrdenInline]
