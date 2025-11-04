b5cee3796a8043ff46beda7a7b8381d55012a5d8
Rinosan25 <rino.kabg@gmail.com>
2025-11-03 21:10:27 -0300

    cambios en app proveedores

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -1,3 +1,4 @@
+# proveedores/urls.py
 from django.urls import path
 from . import views
 
@@ -6,6 +7,6 @@ app_name = "proveedores"
 urlpatterns = [
     path("", views.proveedores_index, name="ficha_proveedores"),
     path("crear/", views.proveedor_crear, name="crear"),
+    path("<int:pk>/editar/", views.proveedor_editar, name="editar"),
+    path("<int:pk>/eliminar/", views.proveedor_eliminar, name="eliminar"),
 ]
-
-
