9b6c5b38651bb85ed901aa7f8aaf7954ab7da59d
Gustavo Muñoz <144866707+HTTPResponseG@users.noreply.github.com>
2025-11-24 00:05:29 -0300

    implementación de modulo de pedidos, generar orden, creación de templates, forms, views, urls y modificación en models OrdenPedido, pequeño ajuste en base html y settings.py para archivos static (#39)

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'pedidos'
+app_name = "pedidos"
 
 urlpatterns = [
-    path('crear/', views.crear_pedido, name='crear_pedido'),
+    path('crear/', views.crear_orden, name='crear'),
+    path('precio-repuesto/<int:repuesto_id>/', views.obtener_precio_repuesto, name='precio_repuesto'),
 ]
+
--- pedidos/views.py
+++ pedidos/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import JsonResponse
+from .forms import OrdenPedidoForm
+from .models import DetalleOrden
+from repuestos.models import Repuesto
+import json
 
-# Create your views here.
-def crear_pedido(request):
-    # Aquí iría la lógica para crear un pedido
-    return render(request, 'pedidos/crear_pedido.html')
+
+def es_mecanico(user):
+    return user.is_authenticated and user.groups.filter(name='Mecanicos').exists()
+
+
+@login_required
+@user_passes_test(es_mecanico)
+def crear_orden(request):
+    form = OrdenPedidoForm()
+    repuestos = Repuesto.objects.all()
+
+    if request.method == "POST":
+        form = OrdenPedidoForm(request.POST)
+
+        if form.is_valid():
+            orden = form.save()
+
+            # Recibir el JSON con detalles enviado desde JS
+            detalles = request.POST.get('detalles_json')
+
+            
+            detalles = json.loads(detalles)
+
+            # Guardar cada detalle en BD
+            for det in detalles:
+                DetalleOrden.objects.create(
+                    orden=orden,
+                    repuesto_id=det['repuesto_id'],
+                    cantidad=det['cantidad'],
+                    precio_unitario=det['precio_unitario'],
+                )
+
+            return redirect('pedidos:lista_ordenes')
+
+    context = {
+        'form': form,
+        'repuestos': repuestos,
+    }
+    return render(request, 'crear_orden.html', context)
+
+@login_required
+@user_passes_test(es_mecanico)
+# AJAX para obtener el precio de compra del repuesto
+def obtener_precio_repuesto(request, repuesto_id):
+    rep = Repuesto.objects.get(id=repuesto_id)
+    return JsonResponse({'precio': rep.precio_compra})
+
+
