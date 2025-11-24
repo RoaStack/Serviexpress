8d7bcd776c50a666a53dde7cde0c4f3cf1fe72ef
Gustavo Muñoz <144866707+HTTPResponseG@users.noreply.github.com>
2025-11-24 20:26:15 -0300

    Feature/pedidos (#44)
    * implementación de modulo de pedidos, generar orden, creación de templates, forms, views, urls y modificación en models OrdenPedido, pequeño ajuste en base html y settings.py para archivos static
    
    * implementación de nuevas views y urls  para poder generar y listar ordenes de pedidos + modificacion de atributo en modelo OrdenPedido

--- pedidos/urls.py
+++ pedidos/urls.py
@@ -5,6 +5,11 @@ app_name = "pedidos"
 
 urlpatterns = [
     path('crear/', views.crear_orden, name='crear'),
+<<<<<<< HEAD
+    path('mis-ordenes/', views.lista_ordenes, name='lista_ordenes'),
+    path('orden/<int:orden_id>/', views.detalle_orden, name='detalle_orden'),
+=======
+>>>>>>> staging
     path('precio-repuesto/<int:repuesto_id>/', views.obtener_precio_repuesto, name='precio_repuesto'),
 ]
 
--- pedidos/views.py
+++ pedidos/views.py
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import JsonResponse
+from .forms import OrdenPedidoForm
+from .models import DetalleOrden, OrdenPedido
+=======
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import JsonResponse
 from .forms import OrdenPedidoForm
 from .models import DetalleOrden
+>>>>>>> staging
 from repuestos.models import Repuesto
 import json
 
@@ -10,17 +18,41 @@ import json
 def es_mecanico(user):
     return user.is_authenticated and user.groups.filter(name='Mecanicos').exists()
 
+<<<<<<< HEAD
+@login_required
+@user_passes_test(es_mecanico)
+def lista_ordenes(request):
+    ordenes = OrdenPedido.objects.filter(mecanico=request.user.perfil).order_by('-fecha_creacion')
+
+    return render(request, 'lista_ordenes.html', {
+        'ordenes': ordenes
+    })
+
+=======
+>>>>>>> staging
 
 @login_required
 @user_passes_test(es_mecanico)
 def crear_orden(request):
+<<<<<<< HEAD
+=======
     form = OrdenPedidoForm()
+>>>>>>> staging
     repuestos = Repuesto.objects.all()
 
     if request.method == "POST":
         form = OrdenPedidoForm(request.POST)
 
         if form.is_valid():
+<<<<<<< HEAD
+            orden = form.save(commit=False)
+            orden.mecanico = request.user.perfil
+            orden.save()
+
+            detalles = request.POST.get('detalles_json')
+            detalles = json.loads(detalles)
+
+=======
             orden = form.save()
 
             # Recibir el JSON con detalles enviado desde JS
@@ -30,6 +62,7 @@ def crear_orden(request):
             detalles = json.loads(detalles)
 
             # Guardar cada detalle en BD
+>>>>>>> staging
             for det in detalles:
                 DetalleOrden.objects.create(
                     orden=orden,
@@ -38,6 +71,29 @@ def crear_orden(request):
                     precio_unitario=det['precio_unitario'],
                 )
 
+<<<<<<< HEAD
+            return redirect('pedidos:detalle_orden', orden_id=orden.id)
+    else:
+        form = OrdenPedidoForm()
+
+    return render(request, 'crear_orden.html', {
+        'form': form,
+        'repuestos': repuestos,
+    })
+
+@login_required
+@user_passes_test(es_mecanico)
+def detalle_orden(request, orden_id):
+    orden = get_object_or_404(OrdenPedido, id=orden_id, mecanico=request.user.perfil)
+
+    detalles = orden.detalles.all()  
+
+    return render(request, "detalle_orden.html", {
+        "orden": orden,
+        "detalles": detalles,
+        "total": orden.monto_total,
+    })
+=======
             return redirect('pedidos:lista_ordenes')
 
     context = {
@@ -45,6 +101,7 @@ def crear_orden(request):
         'repuestos': repuestos,
     }
     return render(request, 'crear_orden.html', context)
+>>>>>>> staging
 
 @login_required
 @user_passes_test(es_mecanico)
