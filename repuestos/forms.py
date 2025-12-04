03c6863aff8a78ce8a8f8af814c2058c4bcda155
GustavoXCODE <gustavo.munoz23@icloud.com>
2025-12-04 18:33:53 -0300

    Estilos a templates de la app repuestos, listar y agregar repuestos y modificación en template de app de boletas atributo repuesto.nombre

--- repuestos/forms.py
+++ repuestos/forms.py
@@ -4,13 +4,37 @@ from .models import Repuesto
 class RepuestoForm(forms.ModelForm):
     class Meta:
         model = Repuesto
-        fields = ['foto','nombre','marca','precio_compra','precio_venta', 'stock', 'limite_stock']
+        fields = ['foto', 'nombre', 'marca', 'precio_compra', 'precio_venta', 'stock', 'limite_stock']
         widgets = {
-            'foto': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre del repuesto'}),
-            'marca': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Marca del repuesto'}),
-            'precio_compra': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
-            'precio_venta': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
-            'stock': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
-            'limite_stock': forms.NumberInput(attrs={'class': 'form-control', 'min': 0}),
+            'foto': forms.ClearableFileInput(attrs={
+                'class': 'form-control'
+            }),
+            'nombre': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nombre del repuesto'
+            }),
+            'marca': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Marca del repuesto'
+            }),
+            'precio_compra': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': 0,
+                'placeholder': 'Precio de compra'
+            }),
+            'precio_venta': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': 0,
+                'placeholder': 'Precio de venta'
+            }),
+            'stock': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': 0,
+                'placeholder': 'Stock disponible'
+            }),
+            'limite_stock': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'min': 0,
+                'placeholder': 'Stock mínimo permitido'
+            }),
         }
