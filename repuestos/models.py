a81604e37bab9cd9e9848b13b7e66553f3f1ff0a
Diego Roa <diegoroa177@gmail.com>
2025-12-09 04:48:58 -0300

    Feature/usuarios (#87)
    * diseño de 3 dashboard, inicio y registro
    
    * cambio imagen dashboard cliente
    
    * ajustes panel admin
    
    * ajustes panel admin
    
    * ajustes de navbars y redirecciones
    
    * ajustes de admin
    
    * ajustes de admin
    
    * Depurando el codigo enigma
    
    * subiendo archivos para despliegue
    
    * actualizacion settings
    
    * actualizacion settings
    
    * anadiendo build.sh y super user
    
    * ajsutes para deploy
    
    * ajsutes para deploy
    
    * ajsutes para deploy
    
    * modificacion en modelo repuesto para clodinary

--- repuestos/models.py
+++ repuestos/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 class Repuesto(models.Model):
-    foto = models.ImageField(upload_to='repuestos/', null=True, blank=True)
+    foto = CloudinaryField('foto', folder='repuestos/',null=True,blank=True)
     nombre = models.CharField(max_length=200)
     marca = models.CharField(max_length=100)
     precio_compra = models.PositiveIntegerField()
